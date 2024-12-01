27e4941036200cb648a7f560f1764c5935e552f6
Jamie Thompson <bishbashboshjt@gmail.com>
2024-12-01 16:23:00 +0100

    correction2

--- 2024-day01.scala
+++ 2024-day01.scala
@@ -5,20 +5,21 @@ import stringmatching.regex.Interpolators.r
 
 def part1(input: String): Int =
   val (is, js) = lists(input)
-  is.sorted.lazyZip(js.sorted).map((l, r) => if r < l then l - r else r - l).sum
+  is.sorted
+    .lazyZip(js.sorted)
+    .map: (l, r) =>
+      if r < l then l - r else r - l
+    .sum
 
 def part2(input: String): Int =
   val (is, js) = lists(input)
 
-  val left = is.groupBy(identity).view.mapValues(_.size).toMap
-  val right = js.groupBy(identity).view.mapValues(_.size).toMap
-
-  left.keys.toArray.map(k => k * right.getOrElse(k, 0)).sum
+  val right = js.groupMapReduce(identity)(_ => 1)(_ + _)
+  is.map(k => k * right.getOrElse(k, 0)).sum
 
 def lists(input: String) =
   val r"${r"$is%d   $js%d"}...(\n)" = input.runtimeChecked
   (left = is, right = js)
-
 
 @main def part1: Unit =
   println(s"the answer is ${part1(inputToday())}")
